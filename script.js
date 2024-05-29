a4cc408648b414e0ab9c695406f67050f51b99f9
SimonDORNIER <simondornier98@gmail.com>
2024-05-29 13:30:55 +0200

    fleche voiture souris

--- script.js
+++ script.js
@@ -1,64 +1,108 @@
-// script.js
-
-class Player {
-    constructor(id, color) {
-        this.id = id;
+class Car {
+    constructor(x, y, angle, color) {
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
         this.color = color;
-        this.position = { x: 100, y: 100 };
-        this.angle = 0;
     }
 
     draw(ctx) {
         ctx.save();
-        ctx.translate(this.position.x, this.position.y);
+        ctx.translate(this.x, this.y);
         ctx.rotate(this.angle);
         ctx.fillStyle = this.color;
-        ctx.fillRect(-10, -5, 20, 10);
+        ctx.fillRect(-10, -5, 20, 10); // Car body
         ctx.beginPath();
-        ctx.arc(10, 0, 5, 0, Math.PI, false);
+        ctx.arc(10, 0, 5, 0, Math.PI, false); // Car nose
         ctx.fill();
         ctx.restore();
     }
 
-    move(distance) {
-        this.position.x += distance * Math.cos(this.angle);
-        this.position.y += distance * Math.sin(this.angle);
+    moveTo(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    rotateTo(angle) {
+        this.angle = angle;
     }
 }
 
-const canvas = document.getElementById('track');
-const ctx = canvas.getContext('2d');
-canvas.width = 600;
-canvas.height = 400;
+class Game {
+    constructor() {
+        this.canvas = document.getElementById('raceTrack');
+        this.ctx = this.canvas.getContext('2d');
+        this.cars = [
+            new Car(400, 300, 0, 'red'),
+            new Car(400, 320, 0, 'blue')
+        ];
+        this.currentPlayer = 0;
+        this.track = this.generateTrack();
+        this.bindEvents();
+        this.render();
+    }
 
-const players = [
-    new Player(1, 'red'),
-    new Player(2, 'blue')
-];
-let currentPlayerIndex = 0;
-let selectedGabarit = 1;
+    generateTrack() {
+        // Placeholder for track generation logic
+        return [];
+    }
 
-document.getElementById('player-number').innerText = players[currentPlayerIndex].id;
+    drawTrack() {
+        // Placeholder for track drawing logic
+    }
 
-function selectGabarit(gabarit) {
-    selectedGabarit = gabarit;
+    bindEvents() {
+        this.canvas.addEventListener('mousemove', (e) => this.showArrow(e));
+        this.canvas.addEventListener('click', (e) => this.moveCar(e));
+    }
+
+    showArrow(event) {
+        const rect = this.canvas.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+
+        const car = this.cars[this.currentPlayer];
+        const dx = x - car.x;
+        const dy = y - car.y;
+        const angle = Math.atan2(dy, dx);
+
+        this.render();
+        this.ctx.beginPath();
+        this.ctx.moveTo(car.x, car.y);
+        this.ctx.lineTo(x, y);
+        this.ctx.strokeStyle = 'black';
+        this.ctx.lineWidth = 2;
+        this.ctx.stroke();
+    }
+
+    moveCar(event) {
+        const rect = this.canvas.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+
+        const car = this.cars[this.currentPlayer];
+        const dx = x - car.x;
+        const dy = y - car.y;
+        const angle = Math.atan2(dy, dx);
+
+        car.moveTo(x, y);
+        car.rotateTo(angle);
+
+        this.nextPlayer();
+        this.render();
+    }
+
+    nextPlayer() {
+        this.currentPlayer = (this.currentPlayer + 1) % this.cars.length;
+    }
+
+    render() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.drawTrack();
+        this.cars.forEach(car => car.draw(this.ctx));
+    }
 }
 
-canvas.addEventListener('click', (e) => {
-    const player = players[currentPlayerIndex];
-    player.move(selectedGabarit * 10);
-    nextTurn();
+document.addEventListener('DOMContentLoaded', () => {
+    const game = new Game();
 });
-
-function nextTurn() {
-    currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
-    document.getElementById('player-number').innerText = players[currentPlayerIndex].id;
-    drawGame();
-}
-
-function drawGame() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    players.forEach(player => player.draw(ctx));
-}
-
-drawGame();
